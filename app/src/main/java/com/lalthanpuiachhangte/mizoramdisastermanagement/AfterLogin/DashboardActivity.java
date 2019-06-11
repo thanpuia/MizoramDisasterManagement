e3d7ad23145c3372cc95cf13182f68eebf0273df
Tpa Chhangte <thanpuia46@gmail.com>
2019-06-11 13:54:21 +0530

    before making a fragment for relief notificaition

--- app/src/main/java/com/lalthanpuiachhangte/mizoramdisastermanagement/AfterLogin/DashboardActivity.java
+++ app/src/main/java/com/lalthanpuiachhangte/mizoramdisastermanagement/AfterLogin/DashboardActivity.java
@@ -68,6 +68,7 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE); getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_dashboard);
+
         Log.d(TAG, "oncreate Starting");
 
         dashboardLayout = findViewById(R.id.dashboardLayout);
